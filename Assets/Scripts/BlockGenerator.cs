31e38f8feab3ce93ad6b7bec5c84586f7db1ad56
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-05-11 09:07:10 -0300

    commit

--- Assets/Scripts/BlockGenerator.cs
+++ Assets/Scripts/BlockGenerator.cs
@@ -2,38 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshCollider))]
-[RequireComponent(typeof(MeshRenderer))]
 public class BlockGenerator : MonoBehaviour
 {
-    int Height = 512;
-
-    int Width = 16;
-
-    Vector2[] NewUV;
-
-    Vector3[] NewVertices;
-
-    Vector3[] Normals;
-
-    int[] NewTriangles;
-
-    Mesh mesh;
-
-    void Awake()
-    {
-        mesh = GetComponent<MeshFilter>().mesh;
-    }
-
-    void Update()
-    {
-        mesh.Clear();
-
-        mesh.vertices = NewVertices;
-
-        mesh.uv = NewUV;
-
-        mesh.triangles = NewTriangles;
-    }
+    
 }
