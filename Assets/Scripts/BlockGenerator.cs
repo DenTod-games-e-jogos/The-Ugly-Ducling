7e42bfa4a5e64e081138c04466ffd90dfdee327f
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-05-11 09:17:30 -0300

    main

--- Assets/Scripts/BlockGenerator.cs
+++ Assets/Scripts/BlockGenerator.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class BlockGenerator : MonoBehaviour
 {
-    
+    public GameObject Tree;
+
+    void Start()
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int z = 0; z < length; z++)
+            {
+                Instantiate(Tree, new Vector3(x, 0, z), Quartenion.identity);
+            }
+        }
+    }
 }
