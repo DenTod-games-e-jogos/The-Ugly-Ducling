65d45f4b85deb0656e2006ea016d1199e4fcca81
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-05-17 14:36:57 -0300

    all

---
+++ Assets/VoxelMaster/Scripts/Examples/LoadingExample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using VoxelMaster;
+
+public class LoadingExample : MonoBehaviour
+{
+    public VoxelTerrain terrain;
+
+    void Start()
+    {
+        if (VoxelTerrain.WorldExists("LoadingExample"))
+        {
+            terrain.LoadWorld("LoadingExample", false);
+        }
+
+        terrain.FastRefresh();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            terrain.SaveWorld("LoadingExample");
+        }
+    }
+}
