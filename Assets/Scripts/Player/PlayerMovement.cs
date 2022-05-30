0347fa739de1c676760c85d3b1190ff2ec32ad50
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-05-30 13:59:32 -0300

    all

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,70 +1,27 @@
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour 
 {
-    CharacterController Controller;
-
-    Rigidbody Rb;
-
     float Speed = 3.0f;
 
     float RotateSpeed = 3.0f;
 
-    Vector3 PlayerVelocity;
-
-    bool GroundedPlayer;
-
-    bool IsJumpPressed = false;
-
-    bool IsMoving = false;
-
-    float JumpHeight = 700.0f;
-
-    float GravityValue = -1.5f;
+    CharacterController Controller;
 
     void Awake()
     {
         Controller = GetComponent<CharacterController>();
-
-        Rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        IsJumpPressed = Input.GetButtonDown("Jump");
-    }
+        transform.Rotate(0, Input.GetAxis("Horizontal") * RotateSpeed, 0);
 
-    void FixedUpdate()
-    {
-        if (GroundedPlayer && PlayerVelocity.y < 0)
-        {
-            PlayerVelocity.y = 0.0f;
-        }
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
 
-        Vector3 Move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        Controller.Move(Move * Time.deltaTime * Speed);
-
-        if (Move != Vector3.zero)
-        {
-            gameObject.transform.forward = Move;
-        }
-
-        if (GroundedPlayer && IsJumpPressed)
-        {
-            Rb.velocity = new Vector3(0, 500, 0);
-
-            PlayerVelocity.y += Mathf.Sqrt(JumpHeight * -3.0f * GravityValue);
-
-            IsMoving = true;
-
-            IsJumpPressed = true;
-        }
-
-        PlayerVelocity.y += GravityValue * Time.deltaTime;
+        float CurSpeed = Speed * Input.GetAxis("Vertical");
         
-        Controller.Move(PlayerVelocity * Time.deltaTime);
+        Controller.SimpleMove(forward * curSpeed);
     }
 }
