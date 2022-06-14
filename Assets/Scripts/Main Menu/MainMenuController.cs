1961d24019f6cf4c7480661cbfe9fd3f2d9463ad
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-06-14 09:07:51 -0300

    all

--- Assets/Scripts/Main Menu/MainMenuController.cs
+++ Assets/Scripts/Main Menu/MainMenuController.cs
@@ -7,6 +7,6 @@ public class MainMenuController : MonoBehaviour
 {
     public void OnNewGameClick()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Game 1");
     }
 }
