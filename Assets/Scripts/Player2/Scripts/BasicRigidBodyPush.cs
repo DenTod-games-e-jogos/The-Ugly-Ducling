af7ac12fd727cd34c050b3437f6f05d9a66a18b5
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-08-01 08:20:02 -0300

    Scripts updated

--- Assets/Scripts/Player2/Scripts/BasicRigidBodyPush.cs
+++ Assets/Scripts/Player2/Scripts/BasicRigidBodyPush.cs
@@ -3,33 +3,43 @@
 public class BasicRigidBodyPush : MonoBehaviour
 {
 	public LayerMask pushLayers;
-	public bool canPush;
-	[Range(0.5f, 5f)] public float strength = 1.1f;
 
-	private void OnControllerColliderHit(ControllerColliderHit hit)
+	public bool canPush;
+	
+	[Range(0.5f, 5f)] 
+	public float strength = 1.1f;
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
 	{
-		if (canPush) PushRigidBodies(hit);
+		if (canPush)
+		{
+			PushRigidBodies(hit);
+		}
 	}
 
-	private void PushRigidBodies(ControllerColliderHit hit)
+	void PushRigidBodies(ControllerColliderHit hit)
 	{
-		// https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
-
-		// make sure we hit a non kinematic rigidbody
 		Rigidbody body = hit.collider.attachedRigidbody;
-		if (body == null || body.isKinematic) return;
 
-		// make sure we only push desired layer(s)
+		if (body == null || body.isKinematic)
+		{
+			return;
+		}
+
 		var bodyLayerMask = 1 << body.gameObject.layer;
-		if ((bodyLayerMask & pushLayers.value) == 0) return;
 
-		// We dont want to push objects below us
-		if (hit.moveDirection.y < -0.3f) return;
+		if ((bodyLayerMask & pushLayers.value) == 0)
+		{
+			return;
+		}
 
-		// Calculate push direction from move direction, horizontal motion only
+		if (hit.moveDirection.y < -0.3f)
+		{
+			return;
+		}
+
 		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
 
-		// Apply the push and take strength into account
 		body.AddForce(pushDir * strength, ForceMode.Impulse);
 	}
 }
