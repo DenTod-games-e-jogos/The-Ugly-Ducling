231b517b5ca631f6ed5b1f11c41d983178765dcc
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-08-12 11:20:47 -0300

    saved

--- Assets/Scripts/Main Menu/MainMenuController.cs
+++ Assets/Scripts/Main Menu/MainMenuController.cs
@@ -14,7 +14,7 @@ public class MainMenuController : MonoBehaviour
 
     IEnumerator StartGame()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.0f);
 
         SceneManager.LoadScene("Game 1");
     }
