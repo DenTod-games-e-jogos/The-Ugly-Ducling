80b8052a56279e125b31868dff2faed76d358167
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-09-05 13:10:15 -0300

    saved

--- Assets/Scripts/Main Menu/MainMenuController.cs
+++ Assets/Scripts/Main Menu/MainMenuController.cs
@@ -3,6 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public Texture2D CursorTexture;
+
+    public CursorMode cursorMode = CursorMode.Auto;
+
+    Vector2 HotSpot = Vector2.zero;
+
+    void Start()
+    {
+        Cursor.SetCursor(CursorTexture, HotSpot, cursorMode);
+    }
+
     public void OnNewGameClick()
     {
         SceneManager.LoadScene("Intro");
