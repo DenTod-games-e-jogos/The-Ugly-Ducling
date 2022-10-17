e2972a03e7c585bbf90a5897600332b69b3fbaf7
marcelo-rossi <gesseemail@gmail.com>
2022-10-17 08:54:44 -0300

    Criado o scritp LanguageDropdown.cs para o controle das opções de idioma no options menu e transformado o Dropdown em prefab

---
+++ Assets/Scripts/Main Menu/LanguageDropdown.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Localization.Settings;
+
+public class LanguageDropdown : MonoBehaviour
+{
+    [SerializeField]
+    public Dropdown languageDropdown;
+    IEnumerator Start()
+    {
+        yield return LocalizationSettings.InitializationOperation;
+        
+        PopulateLanguageDropdown();
+    }
+
+    void PopulateLanguageDropdown()
+    {
+        var options = new List<Dropdown.OptionData>();
+
+        int selected = 0;
+
+        for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i)
+        {
+            var locale = LocalizationSettings.AvailableLocales.Locales[i];
+
+            if (LocalizationSettings.SelectedLocale == locale)
+            {
+                selected = i;
+            }
+
+            options.Add(new Dropdown.OptionData(locale.name));
+        }
+
+        languageDropdown.options = options;
+
+        languageDropdown.value = selected;
+
+        languageDropdown.onValueChanged.AddListener(LocaleSelected);
+    }
+
+    static void LocaleSelected(int index)
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+
+}
--- Assets/Scripts/Main Menu/MainMenuController.cs
+++ Assets/Scripts/Main Menu/MainMenuController.cs
@@ -42,45 +42,11 @@ public class MainMenuController : MonoBehaviour
         Cursor.visible = true;
     }
 
-    IEnumerator Start()
+    void Start()
     {
         Cursor.SetCursor(CursorTexture, HotSpot, CursMode);
 
         GoToMainMenu();
-
-        yield return LocalizationSettings.InitializationOperation;
-
-        PopulateLanguageDropdown();
-    }
-
-    void PopulateLanguageDropdown()
-    {
-        var options = new List<Dropdown.OptionData>();
-
-        int selected = 0;
-        
-        for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i)
-        {
-            var locale = LocalizationSettings.AvailableLocales.Locales[i];
-
-            if (LocalizationSettings.SelectedLocale == locale)
-            {
-                selected = i;
-            }
-
-            options.Add(new Dropdown.OptionData(locale.name));
-        }
-
-        languageDropdown.options = options;
-
-        languageDropdown.value = selected;
-
-        languageDropdown.onValueChanged.AddListener(LocaleSelected);
-    }
-
-    static void LocaleSelected(int index)
-    {
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
     }
 
     public void OnNewGameClick()
