b7b8316d3461c8fd919f10778bb022e18d25c8bf
TheVaxStudio <rodrigomassanori@hotmail.com>
2022-11-28 07:57:39 -0300

    Random namespace using MapStructuresPlacer

--- Assets/Scripts/MapGenerator/MapStructuresPlacer.cs
+++ Assets/Scripts/MapGenerator/MapStructuresPlacer.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MapStructuresPlacer : MonoBehaviour
 {
@@ -22,44 +20,55 @@ public class MapStructuresPlacer : MonoBehaviour
     Vector3 lakeLocal = new Vector3(0.0f, 0.0f, 0.0f);
 
     int mapLimit;
+
     int nBiomes;
+    
     int frontier;
+    
     float radio2;
 
     public float StartAreaRadius { get => startAreaRadius; private set => startAreaRadius = value; }
+    
     public Vector3 StartPoint { get => startPoint; private set => startPoint = value; }
+    
     public float LakeSize { get => lakeSize; private set => lakeSize = value; }
+    
     public Vector3 LakeLocal { get => lakeLocal; private set => lakeLocal = value; }
 
     void Start()
     {
         mapGenerator = GetComponent<MapGenerator>();
+
         mapLimit = mapGenerator.MapLimit;
+        
         nBiomes = mapGenerator.NBiomes;
+        
         frontier = mapGenerator.Frontier;
 
-
-        // Aqui coloca os valores aleatórios do que vai acontecer no pântano roxo
         SetLakeLocalization();
     }
 
-    private void SetLakeLocalization()
+    void SetLakeLocalization()
     {
-        var radio = UnityEngine.Random.Range(MangroveBegin(mapLimit, frontier + lakeSize), MangroveEnd(mapLimit, frontier - lakeSize));
+        var radio = Random.Range(MangroveBegin(mapLimit, frontier + lakeSize), 
+        MangroveEnd(mapLimit, frontier - lakeSize));
+        
         var angle = UnityEngine.Random.Range(0.0f, 360.0f);
+        
         var x = Mathf.RoundToInt(radio * Mathf.Sin(angle * Mathf.Deg2Rad));
+        
         var z = Mathf.RoundToInt(radio * Mathf.Cos(angle * Mathf.Deg2Rad));
+        
         lakeLocal = new Vector3(x, 0, z);
     }
 
     float MangroveBegin(float mapLimit, float frontier)
     {
-
         return (1 * mapLimit / 4) + frontier;
     }
+
     float MangroveEnd(float mapLimit, float frontier)
     {
-
         return (2 * mapLimit / 4) - frontier;
     }
 }
